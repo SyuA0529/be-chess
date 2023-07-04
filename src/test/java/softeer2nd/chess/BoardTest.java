aa8f9e23b0d6c9629d19d56376a55a9cf9b00eaf
SyuA0529 <silbi3408@gmail.com>
2023-07-04 11:59:51 +0900

    refactor: 필요하지 않은 import 문 삭제

--- src/test/java/softeer2nd/chess/BoardTest.java
+++ src/test/java/softeer2nd/chess/BoardTest.java
@@ -1,7 +1,6 @@
 package softeer2nd.chess;
 
 import org.junit.jupiter.api.Test;
-import softeer2nd.chess.Board;
 import softeer2nd.chess.pieces.Pawn;
 
 import static org.assertj.core.api.Assertions.*;
