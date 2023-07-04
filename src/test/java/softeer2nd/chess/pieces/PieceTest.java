88ba99b453281edf299c228b3347248b0d51006f
SyuA0529 <silbi3408@gmail.com>
2023-07-04 18:03:51 +0900

    refactor: 클래스 이름 변경: Pawn -> Piece, Piece 클래스에 팩토리 메소드 추가, 색상 확인 메소드 추가

---
+++ src/test/java/softeer2nd/chess/pieces/PieceTest.java
@@ -0,0 +1,59 @@
+package softeer2nd.chess.pieces;
+
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PieceTest {
+    @Test
+    public void create_piece() {
+        verifyPiece(Piece.createWhitePawn(), Piece.WHITE_COLOR, Piece.WHITE_PAWN_REPRESENTATION);
+        verifyPiece(Piece.createBlackPawn(), Piece.BLACK_COLOR, Piece.BLACK_PAWN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteKnight(), Piece.WHITE_COLOR, Piece.WHITE_KNIGHT_REPRESENTATION);
+        verifyPiece(Piece.createBlackKnight(), Piece.BLACK_COLOR, Piece.BLACK_KNIGHT_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteRook(), Piece.WHITE_COLOR, Piece.WHITE_ROOK_REPRESENTATION);
+        verifyPiece(Piece.createBlackRook(), Piece.BLACK_COLOR, Piece.BLACK_ROOK_REPRESENTATION);
+
+        verifyPiece(Piece.createWhiteBishop(), Piece.WHITE_COLOR, Piece.WHITE_BISHOP_REPRESENTATION);
+        verifyPiece(Piece.createBlackBishop(), Piece.BLACK_COLOR, Piece.BLACK_BISHOP_REPRESENTATION);
+
+        verifyPiece(Piece.createWhithQueen(), Piece.WHITE_COLOR, Piece.WHITE_QUEEN_REPRESENTATION);
+        verifyPiece(Piece.createBlackQueen(), Piece.BLACK_COLOR, Piece.BLACK_QUEEN_REPRESENTATION);
+
+        verifyPiece(Piece.createWhithKing(), Piece.WHITE_COLOR, Piece.WHITE_KING_REPRESENTATION);
+        verifyPiece(Piece.createBlackKing(), Piece.BLACK_COLOR, Piece.BLACK_KING_REPRESENTATION);
+    }
+
+    @Test
+    @DisplayName("기물 색상 검증")
+    void testVerifyPieceColor() {
+        Piece[] whitePieces = {
+                Piece.createWhitePawn(), Piece.createWhiteKnight(), Piece.createWhiteRook(),
+                Piece.createWhiteBishop(), Piece.createWhithQueen(), Piece.createWhithKing()
+        };
+
+        Piece[] blackPieces = {
+                Piece.createBlackPawn(), Piece.createBlackKnight(), Piece.createBlackRook(),
+                Piece.createBlackBishop(), Piece.createBlackQueen(), Piece.createBlackKing()
+        };
+
+        for (Piece whitePiece : whitePieces) {
+            assertThat(whitePiece.isWhite()).isTrue();
+            assertThat(whitePiece.isBlack()).isFalse();
+        }
+
+
+        for (Piece blackPiece : blackPieces) {
+            assertThat(blackPiece.isWhite()).isFalse();
+            assertThat(blackPiece.isBlack()).isTrue();
+        }
+    }
+
+    private void verifyPiece(final Piece piece, final String color, final char representation) {
+        assertEquals(color, piece.getColor());
+        assertEquals(representation, piece.getRepresentation());
+    }
+}
