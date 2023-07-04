3f75fb65663819da23e344631995a60b07506118
SyuA0529 <silbi3408@gmail.com>
2023-07-04 11:24:34 +0900

    feat: 폰 기본 생성자 추가, 체스판 생성 기능 추가

---
+++ src/main/java/softeer2nd/chess/pieces/Pawn.java
@@ -0,0 +1,19 @@
+package softeer2nd.chess.pieces;
+
+public class Pawn {
+    public static final String WHITE_COLOR = "white";
+    public static final String BLACK_COLOR = "black";
+    private final String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public Pawn() {
+        this.color = WHITE_COLOR;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+}
